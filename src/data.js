e45a566ff3abeb8817d7f8f5019270c67684fe45
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-14 13:09:16 +0530

    Question 1 added

---
+++ src/data.js
@@ -0,0 +1,7 @@
+const questions = [
+    {
+        question: "What is the capital of America?",
+        option: ["New York", "Los Angeles", "Washington, D.C.", "Chicago"],
+        answer: "Washington, D.C."
+    },
+]
