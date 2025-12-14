a6cdaaeaeb2ff63834aeef3a42475133bb95973d
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-14 13:09:58 +0530

    Question 2 added

--- src/data.js
+++ src/data.js
@@ -4,4 +4,9 @@ const questions = [
         option: ["New York", "Los Angeles", "Washington, D.C.", "Chicago"],
         answer: "Washington, D.C."
     },
+    {
+        question: "What is the largest planet in our solar system?",
+        option: ["Earth", "Jupiter", "Saturn", "Mars"],
+        answer: "Jupiter"
+    }
 ]
