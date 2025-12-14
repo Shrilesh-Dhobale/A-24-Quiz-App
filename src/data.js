b2b8f0827720427f56a69581d4aca42b63d4c007
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-14 13:11:01 +0530

    Questions, option and ans added

--- src/data.js
+++ src/data.js
@@ -8,5 +8,20 @@ const questions = [
         question: "What is the largest planet in our solar system?",
         option: ["Earth", "Jupiter", "Saturn", "Mars"],
         answer: "Jupiter"
+    },
+    {
+        question: "Who wrote 'Romeo and Juliet'?",
+        option: ["Mark Twain", "William Shakespeare", "Charles Dickens", "Jane Austen"],
+        answer: "William Shakespeare"
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        option: ["O2", "H2O", "CO2", "NaCl"],
+        answer: "H2O"
+    },
+    {
+        question: "Which organ is responsible for pumping blood throughout the body?",
+        option: ["Lungs", "Brain", "Heart", "Liver"],
+        answer: "Heart"
     }
 ]
