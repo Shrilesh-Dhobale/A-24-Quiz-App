03fec8c56fdac9971a1ac452210223ee5e200663
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-15 20:12:47 +0530

    Question option visible

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import questions from './data'
 import './App.css'
 import { ArrowRight } from 'lucide-react';
 
+
 function App() {
   const [questionIndex,SetQuestionIndex] = useState(0);
   const currentQuestion = questions[questionIndex];
@@ -11,6 +12,10 @@ function App() {
     <h1 className='app-head'>Quiz Application</h1>
     <p className="">Questions:{questionIndex+1}</p>
     <p className="question-text">{currentQuestion.question}</p>
+    {currentQuestion.option.map((option,idx)=>(
+      <div key={idx}>{option}</div>
+    ))}
+    
     <ArrowRight 
     className="next-question"
     onClick={()=>{
