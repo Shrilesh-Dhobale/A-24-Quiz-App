a58d99a9ac74eae2e0edba5dcb20a9ac978264ef
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-12-15 20:23:41 +0530

    checkAnswer fun called

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,17 @@ function App() {
   const [questionIndex,SetQuestionIndex] = useState(0);
   const currentQuestion = questions[questionIndex];
   const checkAnswer = (selectedOption) => {};
-  
+
   return (
    <div>
     <h1 className='app-head'>Quiz Application</h1>
     <p className="question-no">Questions:{questionIndex+1}</p>
     <p className="question-text">{currentQuestion.question}</p>
     {currentQuestion.option.map((option,idx)=>(
-      <div key={idx} className='option-card'>{option}</div>
+      <div key={idx} className='option-card' 
+      onClick={()=>
+        checkAnswer(option)
+      }>{option}</div>
     ))}
     
     <ArrowRight 
